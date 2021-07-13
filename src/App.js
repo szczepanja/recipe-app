c495412f83e66239d868b2e2728352920634ecf4
Szczepanik Anja <szczepanikanja@gmail.com>
2021-07-13 19:18:37 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "d2bc2ac0";
   const APP_KEY = "43acd09e440b325d23806e4b6e0addfe";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-  return <div className="App"></div>;
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <h1>recipies</h1>
+    </div>
+  );
 };
 
 export default App;
