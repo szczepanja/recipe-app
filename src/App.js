f7740294fd1acd31b5408d863e194f5589f70551
Szczepanik Anja <szczepanikanja@gmail.com>
2021-07-13 18:59:12 +0200

    environment for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const APP_ID = "d2bc2ac0";
+  const APP_KEY = "43acd09e440b325d23806e4b6e0addfe";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return <div className="App"></div>;
+};
 
 export default App;
